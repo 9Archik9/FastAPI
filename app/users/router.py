d97e0040cfe8de8faafd9f1b97d6ecefe08b46f7
9Archik9 <igro085@gmail.com>
2024-10-31 14:48:08 +0300

    add user login with generate jwt token and set it to cookies

--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status, Response
 
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
-from app.users.auth import get_password_hash
+from app.users.schemas import SUserAuth
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
 
 
 router = APIRouter(
@@ -12,9 +12,20 @@ router = APIRouter(
 
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(email=user_data.email, password=user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    # отправляем jwt токен в coockie браузера c флагом HttpOnly для защиты от JavaScript
+    return access_token
